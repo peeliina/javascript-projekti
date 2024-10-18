14ccf0ec43bf7e6caac1c72cc6b8dea54f144375
Pauliina Laitinen <e2301998@edu.vamk.fi>
2024-10-18 14:29:29 +0300

    Lisätty lisää yksiköitä laajentamaan valintoja

--- script.js
+++ script.js
@@ -14,7 +14,22 @@ function cmToInch(cm) {
 function inchToCm(inch) {
     return inch * 2.54;
 }
+//Lisätty laskut metreille ja litroille
+function metersToFeet(meter) {
+    return meter * 3.2808399;
+}
 
+function feetToMeters(feet) {
+    return feet / 3.2808399;
+}
+
+function literToOunce(liter) {
+    return liter * 35.1950642;
+}
+
+function ounceToLiter(ounce) {
+    return ounce / 35.1950642;
+}
 // Etsitään index.html:stä "convertButton", eli "Muunna"-painike. "click" aktivoituu, kun painiketta klikataan
 document.getElementById('convertButton').addEventListener('click', () => {
 
@@ -46,6 +61,23 @@ document.getElementById('convertButton').addEventListener('click', () => {
         case 'inchToCm':
             result = inchToCm(inputValue).toFixed(2) + ' cm';
             break;
+
+        case 'metersToFeet':
+            result = metersToFeet(inputValue).toFixed(2) + ' feet';
+            break;
+
+        case 'feetToMeters':
+            result = feetToMeters(inputValue).toFixed(2) + ' meters';
+            break;
+
+        case 'literToOunce':
+            result = literToOunce(inputValue).toFixed(2) + ' oz';
+            break;
+
+        case 'ounceToLiter':
+            result = ounceToLiter(inputValue).toFixed(2) + ' l';
+            break;
+        
     }
 
     // Lopuksi lisätään tulos id=result elementtiin index.html-tiedostoon. Tekstissä pitäisi näkyä "Result: ___ lbs/kg/inches/cm"
